18a449119772e5b4172e43bcc5cb7f80bc673b84
georgiusyoga <yogadewantama@gmail.com>
2025-03-27 13:46:56 +0700

    Initial Commit

---
+++ news-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news-app
+//
+//  Created by Georgius Yoga on 27/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
