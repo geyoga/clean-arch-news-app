8efb88a711204b010ac6e2e9fb7957f359d5ce7e
georgiusyoga <yogadewantama@gmail.com>
2025-04-04 15:28:04 +0700

    update Dto response

--- news-app/Data/Network/DataMapping/Repositories/Response/NewsDto.swift
+++ news-app/Data/Network/DataMapping/Repositories/Response/NewsDto.swift
@@ -8,24 +8,24 @@
 import Foundation
 
 struct NewsDto: Decodable {
-    let source: SourceDto
-    let author: String
-    let title: String
-    let description: String
-    let url: String
-    let urlToImage: String
-    let publishedAt: String
-    let content: String
+    let source: SourceDto?
+    let author: String?
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+    let content: String?
 }
 
 extension NewsDto {
     func toDomain() -> News {
         .init(
             id: UUID(),
-            title: title,
-            description: description,
-            imageUrl: urlToImage,
-            source: source.toDomain()
+            title: title ?? "",
+            description: description ?? "",
+            imageUrl: urlToImage ?? "",
+            source: source?.toDomain() ?? .init(id: "", name: "")
         )
     }
 }
